d9e690196e8bebb6beb3d094000e8d5b91473455
Luna <javaspringtea@gmail.com>
2024-08-11 01:02:42 +0800

    add game object

--- Overworld.js
+++ Overworld.js
@@ -9,9 +9,28 @@ class Overworld {
         console.log("Hello from the Overworld", this)
 
         const image = new Image();
+        image.src = "/images/maps/DemoLower.png"
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0);
         }
-        image.src = "/images/maps/DemoLower.png"
+
+
+
+        const x = 1;
+        const y = 4;
+
+        const shadow = new Image()
+        shadow.src = "/images/characters/shadow.png"
+        shadow.onload = () => {
+            this.ctx.drawImage(shadow, 0, 0, 32, 32, x * 16 -8, y * 16 - 18, 32, 32)
+        }
+
+        const hero = new Image();
+        hero.src = "/images/characters/people/hero.png"
+        hero.onload  = () => {
+            this.ctx.drawImage(hero, 0, 0, 32, 32, x * 16 -8, y * 16 - 18, 32, 32)
+
+        }
+
     }
 }
---
+++ Sprite.js
@@ -0,0 +1,20 @@
+class Sprite {
+  constructor(config) {
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    };
+    this.animations = config.animations || {
+      idleDown: [[0, 0]],
+    };
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
+    this.gameObject = config.gameObject;
+  }
+  draw(ctx) {
+    const x = this.gameObject.x * 16 -8;
+    const y = this.gameObject.y * 16 - 18;
+    ctx.drawImage(this.image, 0, 0, 32, 32, x * 16 -8, y * 16 - 18, 32, 32)
+  }
+}
