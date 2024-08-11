6a38b1ab2c85535f506918894e1afe4980c100e6
Luna <javaspringtea@gmail.com>
2024-08-11 18:23:32 +0800

    add overworld maps class

---
+++ OverworldMaps.js
@@ -0,0 +1,51 @@
+class OverworldMaps {
+  constructor(config) {
+    this.gameObject = config.gameObject;
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0);
+  }
+
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0);
+  }
+}
+
+window.overworldMaps = {
+  DemoRoom: {
+    lowerSrc: "/images/maps/DemoLower.png",
+    upperSrc: "/images/maps/DemoUpper.png",
+    gameObject: {
+      hero: new GameObject({
+        x: 5,
+        y: 6,
+      }),
+      npc1: new GameObject({
+        x: 7,
+        y: 9,
+        src: "/images/characters/people/npc1.png",
+      }),
+    },
+  },
+  Kitchen: {
+    lowerSrc: "/images/maps/KitchenLower.png",
+    upperSrc: "/images/maps/KitchenUpper.png",
+    gameObject: {
+      hero: new GameObject({
+        x: 2,
+        y: 1,
+      }),
+      npc1: new GameObject({
+        x: 5,
+        y: 4,
+        src: "/images/characters/people/npc1.png",
+      }),
+    },
+  },
+};
